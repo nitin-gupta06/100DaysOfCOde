574217b432c0a8b98671247b90dca9b0733747c7
nitin-gupta06 <guptanitin7770@gmail.com>
2025-08-23 21:18:18 +0530

    Day2 Programs

---
+++ Day2/Day2_1.c
@@ -0,0 +1,41 @@
+/*
+Q3: Write a program to calculate the area and perimeter of a rectangle given its length and breadth.
+
+Sample Test Cases:
+Input 1:
+5 10
+Output 1:
+Area=50, Perimeter=30
+
+Input 2:
+3 7
+Output 2:
+Area=21, Perimeter=20
+
+*/
+
+#include <stdio.h>
+
+int main() {
+	
+	printf("Program to calculate the area and perimeter of a rectangle given its length and breadth.\n\n");
+	
+	int length, breadth, area, perimeter;
+	printf("Enter lenght of Rectangle: ");
+	scanf("%d", &length);
+	printf("Enter breadth of Rectangle: ");
+	scanf("%d", &breadth);
+	
+	// Area
+	
+	area = length*breadth;
+	
+	// Perimeter
+	
+	perimeter = 2*(length+breadth);
+	
+	printf("Area: %d, Perimeter: %d\n\n", area, perimeter);
+	
+
+	return 0;
+}
---
+++ Day2/Day2_2.c
@@ -0,0 +1,37 @@
+/*
+Q4: Write a program to calculate the area and circumference of a circle given its radius.
+
+Sample Test Cases:
+Input 1:
+7
+Output 1:
+Area=153.94, Circumference=43.96
+
+Input 2:
+3
+Output 2:
+Area=28.27, Circumference=18.85
+
+*/
+
+#include <stdio.h>
+
+int main() {
+
+	printf("Program to calculate the area and circumference of a circle given its radius.\n\n");
+	
+	float radius, area, perimeter;
+	printf("Enter radius of circle: ");
+	scanf("%f", &radius);
+	
+	// area 
+	area = 3.14*radius*radius;
+	
+	// perimeter
+	
+	perimeter = 2*3.14*radius;
+	
+	printf("Area: %f, Perimeter: %f\n\n", area, perimeter);
+	
+	return 0;
+}
