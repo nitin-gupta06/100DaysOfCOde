928dd9a7f933bda8ad1bd2fe2a465e71168593bd
nitin-gupta06 <guptanitin7770@gmail.com>
2025-09-08 00:06:46 +0530

    Day16

---
+++ Ques_21-To-30/Day_16/Day16_1.c
@@ -0,0 +1,32 @@
+/*
+Q31: Write a program to take a number as input and print its equivalent binary representation.
+
+/*
+Sample Test Cases:
+Input 1:
+10
+Output 1:
+1010
+
+Input 2:
+7
+Output 2:
+111
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    printf("Binary representation: ");
+    for (int i = 31; i >= 0; i--) {
+        int bit = (n >> i) & 1;
+        printf("%d", bit);
+    }
+    printf("\n");
+    return 0;
+}
